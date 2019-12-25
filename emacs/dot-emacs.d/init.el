3a3f82ce6db564317004db64976e6286a20e7127
Furkan Usta <furkanusta17@gmail.com>
2019-12-25 22:02:45 +0300

    Add a function to update TODO status depending on the checkboxes

--- emacs/dot-emacs.d/init.el
+++ emacs/dot-emacs.d/init.el
@@ -814,3 +814,25 @@
 ;; (use-package docker-compose-mode :mode ("docker-compose\\.yml\\'" "-compose.yml\\'"))
 
 ;; (use-package docker-tramp)
+
+
+;; (defun ndk/checkbox-list-complete ()
+;;   (save-excursion
+;;     (org-back-to-heading t)
+;;     (let ((beg (point)) end)
+;;       (end-of-line)
+;;       (setq end (point))
+;;       (goto-char beg)
+;;       (if (re-search-forward "\\[\\([0-9]*%\\)\\]\\|\\[\\([0-9]*\\)/\\([0-9]*\\)\\]" end t)
+;;             (if (match-end 1)
+;;                 (if (equal (match-string 1) "100%")
+;;                     ;; all done - do the state change
+;;                     (org-todo 'done)
+;;                   (org-todo 'todo))
+;;               (if (and (> (match-end 2) (match-beginning 2))
+;;                        (equal (match-string 2) (match-string 3)))
+;;                   (org-todo 'done)
+;;                 (org-todo 'todo)))))))
+
+;;          (require 'org-list)
+;; (add-to-list 'org-checkbox-statistics-hook (function ndk/checkbox-list-complete))
