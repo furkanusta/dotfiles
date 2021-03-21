7b799355fd9ea651577a08360c219ecc8e5a5c8c
Furkan Usta <furkanusta17@gmail.com>
2021-03-21 15:24:00 +0300

    Add helpful

--- emacs/dot-emacs.d/init.el
+++ emacs/dot-emacs.d/init.el
@@ -1185,3 +1185,13 @@
 (use-package company-flx
   :after company flx
   :init (company-flx-mode +1))
+
+(use-package helpful
+  :bind
+  ("C-h f" . helpful-callable)
+  ("C-h v" . helpful-variable)
+  ("C-h k" . helpful-key)
+  ("C-h h" . helpful-at-point))
+
+(use-package transient-dwim
+  :bind ("M-=" . transient-dwim-dispatch))
