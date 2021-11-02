eb2889a57ab33e3684d4ad67d3a5dd3f61ffec3c
Furkan Usta <furkanusta17@gmail.com>
2021-11-02 16:00:34 +0300

    Do not use org elpa

--- emacs/dot-emacs.d/init.el
+++ emacs/dot-emacs.d/init.el
@@ -4,7 +4,6 @@
 
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
 (package-initialize)
 
 (require 'server)
