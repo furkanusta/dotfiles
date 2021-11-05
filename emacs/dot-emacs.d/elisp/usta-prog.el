c9c6879780dd739232a74f42189c4077f69a4e14
Furkan Usta <furkanusta17@gmail.com>
2021-11-05 16:02:55 +0300

    Do not ignore compilation mode

--- emacs/dot-emacs.d/elisp/usta-prog.el
+++ emacs/dot-emacs.d/elisp/usta-prog.el
@@ -26,7 +26,7 @@
   :commands persp-current-buffers
   :config (persp-mode t)
   :preface
-  (defvar perspective-skip-ignore-list '("*dashboard*" "*Messages*" "*Warnings*" "*elfeed-search*" "*Fd*"))
+  (defvar perspective-skip-ignore-list '("*dashboard*" "*Messages*" "*Warnings*" "*elfeed-search*" "*Fd*" "*compilation*"))
   (defvar perspective-skip-prefix-list '("magit-"))
   (defvar perspective-skip-ignore-prefix-list '("*vterm" "*scratch" "*shell" "*Customize" "*ielm*" "*helpful" "*org" "*ein"))
   (defun perspective-my-skip-buffer-p (window buffer burry-or-kill)
