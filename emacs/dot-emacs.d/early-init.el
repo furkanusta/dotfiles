c4ff14700ddc7e45638860a33559e4293473faa4
Furkan Usta <furkanusta17@gmail.com>
2021-11-24 21:41:08 +0300

    Fix early-init

--- emacs/dot-emacs.d/early-init.el
+++ emacs/dot-emacs.d/early-init.el
@@ -1,18 +1,10 @@
 ;; (setq-default package-enable-at-startup nil)
 
+(setq gc-cons-threshold 64000000
+      gc-cons-percentage 0.8
+      package-enable-at-startup nil
+      load-prefer-newer t)
 
-(let ((default-gc-threshold gc-cons-threshold)
-      (default-gc-percentage gc-cons-percentage))
-  (setq gc-cons-threshold most-positive-fixnum
-        gc-cons-percentage 0.8)
-  (add-hook 'after-init-hook
-            (lambda ()
-              (setq gc-cons-percentage default-gc-percentage
-                    gc-cons-threshold default-gc-threshold))))
-
-(setq load-prefer-newer t)
-
-(setq package-enable-at-startup nil)
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
