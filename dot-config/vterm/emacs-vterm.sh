0ddc6baa4a05b9570853b82e0a69dc9472346094
Furkan Usta <furkanusta17@gmail.com>
2022-06-04 13:01:39 +0300

    Fix vterm prompt

--- dot-config/vterm/emacs-vterm.sh
+++ dot-config/vterm/emacs-vterm.sh
@@ -38,6 +38,11 @@ vterm_cmd() {
     vterm_printf "51;E$vterm_elisp"
 }
 
+# This is to change the title of the buffer based on information provided by the
+# shell. See, http://tldp.org/HOWTO/Xterm-Title-4.html, for the meaning of the
+# various symbols.
+PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND; }"'echo -ne "\033]0;${HOSTNAME}:${PWD}\007"'
+
 # Sync directory and host in the shell with Emacs's current directory.
 # You may need to manually specify the hostname instead of $(hostname) in case
 # $(hostname) does not return the correct string to connect to the server.
@@ -45,6 +50,6 @@ vterm_cmd() {
 # The escape sequence "51;A" has also the role of identifying the end of the
 # prompt
 vterm_prompt_end(){
-    # vterm_printf "51;A$PS1"
-    vterm_printf "51;A$(pwd)";
+    vterm_printf "51;A$(whoami)@$(hostname):$(pwd)"
 }
+PS1=$PS1'\[$(vterm_prompt_end)\]'
